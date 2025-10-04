b30171479fd96a9288ee094cee72046486f8dc28
Hemanth Vundavilli <118917390+HemanthVundavilli@users.noreply.github.com>
2025-10-04 15:43:42 +0530

    axios.js

--- src/components/pages/api/axios.js
+++ src/components/pages/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token'); // or wherever you store your JWT token
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://school-application-backend.onrender.com//api',
   headers: {
     Authorization: `Bearer ${token}`,
   },
